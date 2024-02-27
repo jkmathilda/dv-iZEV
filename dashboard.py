74adb6eb2331a7439b1f17d95781e584d20b1d17
jkmathilda <jkmathilda627@gmail.com>
2024-02-27 00:54:53 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,22 +9,24 @@ st.header("📊 Data Visualization: Incentives for Zero-Emission Vehicles (iZEV)
 
 df = pd.read_csv("izev.csv", skipinitialspace=True)
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-df = df.sort_values('Incentive Request Date')
+df.columns = df.columns.str.strip()
+# df = df.sort_values('Incentive Request Date')
 
 # 1. Car count per province % Month and year
 col11, col12, col13 = st.columns([2, 6, 1])
 with col12: 
     print(df.columns)
-    car_count_per_month = df['Month and Year '].value_counts().reset_index()
-    car_count_per_month.columns = ['Month and Year ', 'Number of Vehicles']
-    df_aggregated = df.groupby('Month and Year ').size().reset_index(name='Number of Vehicles')
+    car_count_per_month = df['Month and Year'].value_counts().reset_index()
+    car_count_per_month.columns = ['Month and Year', 'Number of Vehicles']
+    df_aggregated = df.groupby(['Month and Year', 'Recipient Province / Territory']).size().reset_index(name='Number of Vehicles')
+    print(df_aggregated.columns)
     fig = px.line(
         df_aggregated,
         title="Number of Vehicles per Province",
-        x="Month and Year ", 
+        x="Month and Year", 
         y="Number of Vehicles", 
-        # color="Recipient Province / Territory ", 
-        # line_group="Recipient Province / Territory ", 
+        color="Recipient Province / Territory", 
+        line_group="Recipient Province / Territory", 
         line_shape="spline", 
         render_mode="svg"
     )
