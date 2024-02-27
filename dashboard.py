ceca61aaa22ccd1695a7c2c797003714e70f96fb
jkmathilda <jkmathilda627@gmail.com>
2024-02-27 00:48:25 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,22 +5,56 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 st.set_page_config(page_title="iZEV Dashboard", layout="wide")
-st.header("📊 Data Visualization: Incentives for Zero-Emission Vehicles (iZEV) Program FY2019-20")
+st.header("📊 Data Visualization: Incentives for Zero-Emission Vehicles (iZEV) Program FY2019-23")
 
 df = pd.read_csv("izev.csv", skipinitialspace=True)
 df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+df = df.sort_values('Incentive Request Date')
 
-col1, col2 = st.columns(2)
+# 1. Car count per province % Month and year
+col11, col12, col13 = st.columns([2, 6, 1])
+with col12: 
+    print(df.columns)
+    car_count_per_month = df['Month and Year '].value_counts().reset_index()
+    car_count_per_month.columns = ['Month and Year ', 'Number of Vehicles']
+    df_aggregated = df.groupby('Month and Year ').size().reset_index(name='Number of Vehicles')
+    fig = px.line(
+        df_aggregated,
+        title="Number of Vehicles per Province",
+        x="Month and Year ", 
+        y="Number of Vehicles", 
+        # color="Recipient Province / Territory ", 
+        # line_group="Recipient Province / Territory ", 
+        line_shape="spline", 
+        render_mode="svg"
+    )
+    # fig = px.line(
+    #     df,
+    #     car_count_per_month,
+    #     x="Incentive Request Date", 
+    #     y="Number of Vehicles", 
+    #     color="Recipient Province / Territory", 
+    #     line_group="Recipient Province / Territory", 
+    #     line_shape="spline", 
+    #     render_mode="svg"
+    # )
+    st.plotly_chart(fig)
+
 
 # 2. Vehicle brands & model count (month and year)
-with col1:
-    fig = px.sunburst(df, 
-                      title="Vehicle Make & Model Count",
-                      path=['Vehicle Make', 'Vehicle Model'])
+col21, col22 = st.columns(2)
+with col21:
+    fig = px.sunburst(
+        df, 
+        title="Vehicle Make & Model Count",
+        path=['Vehicle Make', 'Vehicle Model']
+    )
     st.plotly_chart(fig)
     
-with col2: 
-    st.write("Types of Electric Vehicle: BEV, PHEV, FCEV")
-    fig = px.sunburst(df,
-                      path=['Battery-Electric Vehicle (BEV), Plug-in Hybrid Electric Vehicle (PHEV) or Fuel Cell Electric Vehicle (FCEV)', 'Vehicle Make'])
+with col22: 
+    fig = px.sunburst(
+        df,
+        title="Types of Electric Vehicle: BEV, PHEV, FCEV",
+        path=['Battery-Electric Vehicle (BEV), Plug-in Hybrid Electric Vehicle (PHEV) or Fuel Cell Electric Vehicle (FCEV)', 'Vehicle Make']
+    )
     st.plotly_chart(fig)
